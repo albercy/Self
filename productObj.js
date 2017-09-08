89cc91b169c614500d51dea00c2c4f5c9c2b30f8
Tim <18318741662@163.com>
2017-09-08 17:36:18 +0800

    实际测试

---
+++ productObj.js
@@ -0,0 +1,27 @@
+var getProduct = {
+    productObj: function(){
+        var mObj = [
+            {
+                productName: '高丽参(同和堂) (天)字37.5g*30支 广东天诚中药饮片有限公司',
+                productPrice: '10.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '高丽参(同和堂) (天)字37.5g*30支 广东天诚中药饮片有限公司',
+                productPrice: '10.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '高丽参(同和堂) (天)字37.5g*30支 广东天诚中药饮片有限公司',
+                productPrice: '10.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '高丽参(同和堂) (天)字37.5g*30支 广东天诚中药饮片有限公司',
+                productPrice: '10.00',
+                productImg: 'image/photo.jpg'
+            }
+        ]
+        return mStr
+    }
+}
