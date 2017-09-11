938afa0661285741cf5e9220df17a6409e2e64ae
Tim <18318741662@163.com>
2017-09-11 17:37:31 +0800

    翻页新逻辑

--- productObj.js
+++ productObj.js
@@ -3,25 +3,105 @@ var getProduct = {
         var mObj = [
             {
                 productName: '高丽参(同和堂) (天)字37.5g*30支 广东天诚中药饮片有限公司',
+                productPrice: '28.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '小儿止咳糖浆 150ml 雅安三九药业有限公司',
                 productPrice: '10.00',
                 productImg: 'image/photo.jpg'
             },
             {
-                productName: '高丽参(同和堂) (天)字37.5g*30支 广东天诚中药饮片有限公司',
+                productName: '香雪 抗病毒口服液 10ml*10支 广州市香雪制药股份有限公司',
+                productPrice: '12.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '万通筋骨贴 7cm*10cm*6贴 通化万通药业股份有限公司',
+                productPrice: '15.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '999 抗病毒口服液 10ml*10支 杭州老桐君制药有限公司',
+                productPrice: '12.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '阿胶(铁盒简装) 500g 山东东阿阿胶股份有限公司',
+                productPrice: '800.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '999 正天丸 6g*10袋 华润三九医药股份有限公司',
+                productPrice: '20.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '万通 感通片 0.32g*20片 通化万通药业股份有限公司',
+                productPrice: '15.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '999 抗病毒口服液 10ml*10支 杭州老桐君制药有限公司',
+                productPrice: '12.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '999皮炎平 复方醋酸地塞米松乳膏 20g 华润三九医药股份有限公司 ',
                 productPrice: '10.00',
                 productImg: 'image/photo.jpg'
             },
             {
-                productName: '高丽参(同和堂) (天)字37.5g*30支 广东天诚中药饮片有限公司',
+                productName: '樟冰油(强力狮子油) 25ml 英吉利制药厂有限公司',
+                productPrice: '20.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '顺峰康霜 曲咪新乳膏 10g 广东华润顺峰药业有限公司',
+                productPrice: '4.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '肾宝糖浆 10ml*10支 江西美媛春药业股份有限公司',
+                productPrice: '30.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '香雪 抗病毒口服液 10ml*12支 广州市香雪制药股份有限公司',
+                productPrice: '20.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '顺峰康王 酮康他索乳膏 10g 广东华润顺峰药业有限公司',
+                productPrice: '6.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '六味地黄丸(浓缩丸) 120丸 北京同仁堂科技发展股份有限公司制药厂',
                 productPrice: '10.00',
                 productImg: 'image/photo.jpg'
             },
             {
-                productName: '高丽参(同和堂) (天)字37.5g*30支 广东天诚中药饮片有限公司',
+                productName: '阿莫西林胶囊 0.125g*50粒 佛山手心制药有限公司',
+                productPrice: '15.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '腹可安片(OTC) 薄膜衣片/0.34g*24片 广州白云山中一药业有限公司',
+                productPrice: '3.50',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '三九 小儿感冒颗粒 6g*10袋 华润三九(枣庄)药业有限公司',
+                productPrice: '12.00',
+                productImg: 'image/photo.jpg'
+            },
+            {
+                productName: '思密达 蒙脱石散(桔子味) 3g*10袋 博福-益普生(天津)制药有限公司',
                 productPrice: '10.00',
                 productImg: 'image/photo.jpg'
             }
         ]
-        return mStr
+        return mObj
     }
 }
