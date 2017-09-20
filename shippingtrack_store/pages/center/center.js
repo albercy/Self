b4b706b51e98dc6423d7bca43f6138bc90ad747c
Tim <18318741662@163.com>
2017-09-20 17:35:39 +0800

    小程序翻页

--- shippingtrack_store/pages/center/center.js
+++ shippingtrack_store/pages/center/center.js
@@ -1,6 +1,16 @@
 Page({
   data: {
+    last: '<',
+    next: '>',
     pageArr: [1, 2, 3]
   },
-  onLoad: function (options) {}
+  onLoad: function (options) {
+    var pageArr = []
+    for(var i=0; i<10; i++){
+      pageArr.push(i+1)
+    }
+    this.setData({
+      pageArr: pageArr
+    })
+  }
 })
